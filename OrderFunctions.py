8d7d2d5c3db66d949d17843497f29258c5eb8472
Linas Jurkauskas <tiesioglinas@gmail.com>
2020-02-04 22:45:45 +0200

    black formatter used

--- OrderFunctions.py
+++ OrderFunctions.py
@@ -1,11 +1,12 @@
 def session_reorder(player_list):
     for player in player_list:
-        if player[2] == len(player_list)-1:
+        if player[2] == len(player_list) - 1:
             player[2] = 0
         else:
             player[2] += 1
-    player_list = sorted(player_list, key=lambda x: x[2], reverse=False)   
+    player_list = sorted(player_list, key=lambda x: x[2], reverse=False)
     return player_list
+
 
 def round_reorder(player_list, winner):
     i = 0
@@ -27,57 +28,57 @@ def round_reorder(player_list, winner):
     return player_list
 
 
-def score_updater(player_list,bid_list):
-    '''
+def score_updater(player_list, bid_list):
+    """
     Rewards players with correct bids.
     Takes away points for players who missed their bids.
-    '''
+    """
     for player in player_list:
         player_name = player[0]
-        if bid_list[player_name][0] ==  bid_list[player_name][1]:
-            player[1] = player[1] + (bid_list[player_name][0]*20)+20
+        if bid_list[player_name][0] == bid_list[player_name][1]:
+            player[1] = player[1] + (bid_list[player_name][0] * 20) + 20
         else:
-            diff = (abs(bid_list[player_name][0] - bid_list[player_name][1]))*10
+            diff = (abs(bid_list[player_name][0] - bid_list[player_name][1])) * 10
             player[1] = player[1] - diff
     return player_list
 
 
-def find_winner(winner, placed_card, round_colour,dominant_colour):
-    '''
+def find_winner(winner, placed_card, round_colour, dominant_colour):
+    """
     Checks if newly placed card is better than current winner card and replaces if so.
-    '''
-    if  winner[0] == 'None':
-        #first card in round, by default becomes winner.
+    """
+    if winner[0] == "None":
+        # first card in round, by default becomes winner.
         winner[0] = placed_card[0]
         winner[1] = placed_card[1]
         winner[2] = placed_card[2]
-    elif placed_card[2] == 50 and winner[2] != 50: 
-        #user has wizard card and it has not been placed in this round yet.
+    elif placed_card[2] == 50 and winner[2] != 50:
+        # user has wizard card and it has not been placed in this round yet.
         winner[0] = placed_card[0]
         winner[1] = placed_card[1]
         winner[2] = placed_card[2]
-    elif int(placed_card[2])  == 0 and int(placed_card[2]) == winner[2]:
-        #user has fool card but it already has been placed.New placer becomes winner.
+    elif int(placed_card[2]) == 0 and int(placed_card[2]) == winner[2]:
+        # user has fool card but it already has been placed.New placer becomes winner.
         winner[0] = placed_card[0]
         winner[1] = placed_card[1]
         winner[2] = placed_card[2]
-    elif dominant_colour in placed_card[1] and int(placed_card[2])> winner[2]:
-        #user has session colour card that is higher than current winner score
+    elif dominant_colour in placed_card[1] and int(placed_card[2]) > winner[2]:
+        # user has session colour card that is higher than current winner score
         winner[0] = placed_card[0]
         winner[1] = placed_card[1]
         winner[2] = placed_card[2]
     elif round_colour in placed_card[1] and int(placed_card[2]) > winner[2]:
-        #user has round colour card that is higher than current winner score
+        # user has round colour card that is higher than current winner score
         winner[0] = placed_card[0]
         winner[1] = placed_card[1]
         winner[2] = placed_card[2]
-    
+
     return winner
 
-   
-def check_round_dominant(round_colour,card):
-    if round_colour == 'None' and card.find('[') != -1:
-        round_colour = card.split(']')[0].replace('[','')
-    else: 
+
+def check_round_dominant(round_colour, card):
+    if round_colour == "None" and card.find("[") != -1:
+        round_colour = card.split("]")[0].replace("[", "")
+    else:
         round_colour
     return round_colour
